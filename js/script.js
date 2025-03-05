eac0b82385c2500abbe8ce23897808d4b106bf33
Andre Kilesse <kilesseandre@gmail.com>
2025-03-04 21:50:00 -0300

    First Commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const form = document.querySelector('.hero__form');
+const alertBox = document.querySelector(".custom-alert");
+const closeAlert = document.querySelector(".custom-alert__button");
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const emailInput = document.querySelector('.inEmail');
+    const errorIcon = document.querySelector('.hero__form--icon-error');
+    const errorMsg = document.querySelector('.hero__form--text-error');
+
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    let isValid = emailPattern.test(emailInput.value);
+
+    if (!isValid) {
+        emailInput.style.borderColor = 'red';
+        errorIcon.style.display = 'block';
+        errorMsg.style.display = 'block';
+        return;
+    }
+
+    alertBox.classList.add('show');
+
+    emailInput.value = "";
+    emailInput.style.borderColor = '';
+    errorIcon.style.display = 'none';
+    errorMsg.style.display = 'none';
+})
+
+closeAlert.addEventListener('click', () => {
+    alertBox.classList.remove('show');
+})
